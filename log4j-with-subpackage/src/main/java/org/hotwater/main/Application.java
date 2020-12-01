9071073822fd963fc987475074da79ec0fca0a27
Israel Altamira <urkunde22@gmail.com>
2020-12-01 00:09:27 -0600

    adding some structural changes to new log4j-with-subpackage sample

---
+++ log4j-with-subpackage/src/main/java/org/hotwater/main/Application.java
@@ -0,0 +1,22 @@
+package org.hotwater.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * 
+ * */
+@SpringBootApplication
+public class Application {
+
+	//
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		Logger logger = LogManager.getLogger();
+		logger.info("This is a father message.");
+	}
+
+}
