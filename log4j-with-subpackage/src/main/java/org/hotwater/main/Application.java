49831ad6aacee4d9903a5f03c5e0817c474fcd66
Israel Altamira <urkunde22@gmail.com>
2020-12-01 00:45:21 -0600

    complete the experiment with a packageA importing a packageB and log4j was imported only in packageA

--- log4j-with-subpackage/src/main/java/org/hotwater/main/Application.java
+++ log4j-with-subpackage/src/main/java/org/hotwater/main/Application.java
@@ -14,14 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	// 1) Try to run my subpackage class, the which one throws 1 log event.
-	// PRECONDITION 		importing org.anothercompany.sample.MainApplication; (i did it!)
-	// EXPECTED				the app shows in console the log lines from both Apps
-	// RESULT
+	// PRECONDITION ----importing org.anothercompany.sample.MainApplication;
+	// (i did it!)
+	// EXPECTED --------the app shows in console log lines from both Apps
+	// RESULT ----------the app shows in console log lines from both Apps!!
+	// and subpackage only needs to import log4j-core artifact for enable
+	// compilation time
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		Logger logger = LogManager.getLogger();
 		logger.info("This is a father message.");
-		
+
 		MainApplication app = new MainApplication();
 		app.run();
 	}
